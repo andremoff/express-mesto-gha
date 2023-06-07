370a57f03ed9a09b9b413f1f68ac2278d1939c8e
Andrey Dremov <andrem0ff@yandex.ru>
2023-06-07 19:15:45 +0300

    P13 backend

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const { getCards, createCard, deleteCard } = require('../controllers/cards');
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} = require('../controllers/cards');
 
 const cardsRouter = express.Router();
 
 cardsRouter.get('/', getCards);
 cardsRouter.post('/', createCard);
 cardsRouter.delete('/:cardId', deleteCard);
+cardsRouter.put('/:cardId/likes', likeCard);
+cardsRouter.delete('/:cardId/likes', dislikeCard);
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const { getUsers, getUserById, createUser } = require('../controllers/users');
+const {
+  getUsers,
+  getUserById,
+  updateUser,
+  updateAvatar,
+  createUser,
+} = require('../controllers/users');
 
 const usersRouter = express.Router();
 
 usersRouter.get('/', getUsers);
 usersRouter.get('/:userId', getUserById);
+usersRouter.patch('/me', updateUser);
+usersRouter.patch('/me/avatar', updateAvatar);
 usersRouter.post('/', createUser);
 
 module.exports = usersRouter;
